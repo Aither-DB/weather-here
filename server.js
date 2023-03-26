59996159e6f8a8e3c432d49854cca83e3d71c138
Aither-DB <122840576+Aither-DB@users.noreply.github.com>
2023-03-26 14:34:50 +1030

    Port from environment variable

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const Datastore = require("nedb");
 require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, ()=> console.log(`This site is running on port ${port}`));
 app.use(express.static("public"));
 app.use(express.json( {limit: "1mb"}));
